1541e948d05d8260297bf6b702046c74e71294ca
Nicola De Filippo <nicola@nicoladefilippo.it>
2024-05-04 20:05:24 +0200

    Initial Commit

---
+++ WriteNfcTag/WriteNfcTagApp.swift
@@ -0,0 +1,17 @@
+//
+//  WriteNfcTagApp.swift
+//  WriteNfcTag
+//
+//  Created by Nicola De Filippo on 04/05/24.
+//
+
+import SwiftUI
+
+@main
+struct WriteNfcTagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
